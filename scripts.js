a5b0c661d61625add8b7f7cbcb3f9e9efd607124
Dawidd95 <32812931+Dawidd95@users.noreply.github.com>
2019-05-24 13:15:52 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,31 @@
+// $('#myTab a').on('click', function (e) {
+//    e.preventDefault()
+//    $(this).tab('show')
+//  })
+
+//  $('#myTab a[href="#profile"]').tab('show') // Select tab by name
+// $('#myTab li:first-child a').tab('show') // Select first tab
+// $('#myTab li:last-child a').tab('show') // Select last tab
+// $('#myTab li:nth-child(3) a').tab('show') // Select third tab
+
+// let firstLink = document.querySelector('.nav-link--first');
+// let secondLink = document.querySelector('..nav-link--second');
+// let thirdLink = document.querySelector('.nav-link--third');
+
+// firstLink.addEventListener('click', () => {
+//    firstLink.style.display ='flex';
+//    secondLink.style.display = 'none';
+//    thirdLink.style.display= 'none';
+// })
+
+// secondLink.addEventListener('click', () => {
+//    firstLink.style.display ='none';
+//    secondLink.style.display = 'flex';
+//    thirdLink.style.display= 'none';
+// })
+
+// thirdLink.addEventListener('click', () => {
+//    firstLink.style.display ='none';
+//    secondLink.style.display = 'none';
+//    thirdLink.style.display= 'flex';
+// })
